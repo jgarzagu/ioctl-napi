9342c741c5f3ab8c74c65f9f7c6d47276b3056d3
jorge <jorgealbertogarza@gmail.com>
2019-10-31 01:20:44 -0700

    Added memAddress to get memory address of javascript buffers

--- ioctl.cc
+++ ioctl.cc
@@ -1,7 +1,8 @@
 #include <node_api.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
+#include <inttypes.h>
 #include <sys/ioctl.h>
 
 // NAPI METHOD: napi_ioctl
@@ -21,26 +22,26 @@ napi_value napi_ioctl (napi_env env, napi_callback_info info) {
     return NULL;
   }
 
-  // Get fd value
+  // Get fd (argument 1) value 
   int32_t fd;
   if (napi_get_value_int32(env, argv[0], &fd) != napi_ok) {
     napi_throw_error(env, "EINVAL", "Expected number");
     return NULL;
   }
 
-  // Get request value
+  // Get request (argument 2) value 
   uint32_t request;
   if (napi_get_value_uint32(env, argv[1], &request) != napi_ok) { \
     napi_throw_error(env, "EINVAL", "Expected unsigned number"); \
     return NULL;
   }
 
-  // Get argp value type (int or object)
+  // Get argp (argument 3) value type (int or object)
   napi_valuetype valuetype2;
   napi_typeof(env, argv[2], &valuetype2);
-
   if (valuetype2 == napi_number) {
     // napi_number
+    // Get argp (argument 3) value as integer
     int32_t argp;
     if (napi_get_value_int32(env, argv[2], &argp) != napi_ok) {
       napi_throw_error(env, "EINVAL", "Expected number");
@@ -49,59 +50,131 @@ napi_value napi_ioctl (napi_env env, napi_callback_info info) {
     rc = ioctl(fd, request, argp);
   } else {
     // napi_object
+    // Get argp (argument 3) value as object
     void * argp;
     size_t argp_len;
-    if (napi_get_buffer_info(env, argv[2], (void **) &argp, &argp_len) != napi_ok) {
+    if (napi_get_buffer_info(env, 
+      argv[2], (void **) &argp, &argp_len) != napi_ok) {
       napi_throw_error(env, NULL, "napi_get_buffer_info() failed");
       return NULL;
     }
     rc = ioctl(fd, request, argp);
   }
 
-  // return error if less than 0
+  // Return error if ioctl result is less than 0
   if (rc < 0) {
     sprintf(code, "%d", errno);
     napi_throw_error(env,code,(const char *) strerror(errno));
   }
 
-  // retrun value
+  // Retrun ioctl result
   napi_value return_int32;
   napi_create_int32(env, rc, &return_int32);
   return return_int32;
 }
 
-// NAPI INIT
-static void napi_macros_init(napi_env env, napi_value exports);
-napi_value napi_macros_init_wrap (napi_env env, napi_value exports) {
-  napi_macros_init(env, exports);
-  return exports;
-} 
-NAPI_MODULE(NODE_GYP_MODULE_NAME, napi_macros_init_wrap);
-static void napi_macros_init (napi_env env, napi_value exports) {
-  napi_value napi_ioctl_function;
+// NAPI METHOD: napi_memAddress
+napi_value napi_memAddress (napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value argv[1];
+  napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
+  char *memAddress;
+  
+  // Check if there is 1 argument
+  if (argc < 1) {
+    napi_throw_error(
+      env,
+      NULL,
+      "Wrong number of arguments. Expected 1");
+    return NULL;
+  }
 
-  if ( 
-    napi_create_function(
+  // Check if argument 1 is a Buffer
+  bool is_buffer;
+  napi_is_buffer(env, argv[0], &is_buffer);
+  if (!is_buffer) {
+    napi_throw_error(
+      env,
+      NULL,
+      "Buffer instance expected");
+    return NULL;
+  }
+
+  // Get Buffer
+  char * argp;     // Argument pointer (Buffer is cast to char *)
+  size_t argp_len; // Argument length  (Buffer length result)
+  if (napi_get_buffer_info(env, argv[0], 
+      (void **) &argp, &argp_len) != napi_ok) {
+    napi_throw_error(env, NULL, "napi_get_buffer_info() failed");
+    return NULL;
+  }
+
+  // Get Buffer address 
+  // (Should print 16 digits if 64-bit arch and 8 digits if 32-bit arch)
+  int ret = asprintf(&memAddress, "%0*" PRIxPTR, 
+    (int)sizeof(uintptr_t)*2, (uintptr_t)argp);
+  if (ret < 0) {
+    napi_throw_error(env, NULL, 
+      "asprintf() failed, error allocating memory for address string");
+    return NULL;
+  }
+
+  // Return Buffer address as string
+  napi_value return_utf8;
+  napi_create_string_utf8(env, memAddress, strlen(memAddress), &return_utf8);
+  return return_utf8;
+}
+
+// NAPI INIT
+napi_value Init(napi_env env, napi_value exports) {
+  napi_status status;
+  napi_value napi_function;
+
+  // Create ioctl function
+  status = napi_create_function(
     env,
     "ioctl",
     NAPI_AUTO_LENGTH,
     napi_ioctl,
     NULL,
-    &napi_ioctl_function) != napi_ok) 
-  {
+    &napi_function);
+  if ( status != napi_ok) {
     napi_throw_error(env, NULL, "napi_create_function() failed!");
-    return;
   }
 
-  if (
-    napi_set_named_property(
+  // Set ioctl function in exports
+  status = napi_set_named_property(
     env,
     exports,
     "ioctl",
-    napi_ioctl_function) != napi_ok) 
-  {
+    napi_function);
+  if (status != napi_ok) {
     napi_throw_error(env, NULL, "napi_set_named_property() failed!");
-    return;
   }
 
+  // Create memAddress function
+  status = napi_create_function(
+    env,
+    "memAddress",
+    NAPI_AUTO_LENGTH,
+    napi_memAddress,
+    NULL,
+    &napi_function);
+  if ( status != napi_ok) {
+    napi_throw_error(env, NULL, "napi_create_function() failed!");
+  }
+
+  // Set memAddress function in exports
+  status = napi_set_named_property(
+    env,
+    exports,
+    "memAddress",
+    napi_function);
+  if (status != napi_ok) {
+    napi_throw_error(env, NULL, "napi_set_named_property() failed!");
+  }
+
+  return exports;
 }
+
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Init);
--- ioctl.js
+++ ioctl.js
@@ -1,4 +1,34 @@
-var ioctl = require('bindings')('ioctl').ioctl;
+const os = require('os');
 
-module.exports = ioctl
+// Export ioctl
+module.exports = require('bindings')('ioctl').ioctl;
 
+// Export memAddress functions
+const bufferMemAddress = require('bindings')('ioctl').memAddress;
+Buffer.prototype.memAddress = function memAddress () {
+  return bufferMemAddress(this);
+};
+Buffer.prototype.memAddressLE = function memAddressLE () {
+	if (os.endianness() == 'LE') {
+		return swapMemAddress(bufferMemAddress(this));
+	} else {
+		return bufferMemAddress(this);
+	}
+};
+Buffer.prototype.memAddressBE = function memAddressLE () {
+	if (os.endianness() == 'BE') {
+		return bufferMemAddress(this);	
+	} else {
+		return swapMemAddress(bufferMemAddress(this));
+	}
+};
+function swapMemAddress (a) {
+	if(a.length == 16) { // 64-bit architecture 
+		return (a[14]+a[15]+a[12]+a[13]+a[10]+a[11]+a[8]+a[9]+
+			a[6]+a[7]+a[4]+a[5]+a[2]+a[3]+a[0]+a[1]);
+	} else if (a.length == 8) { // 32-bit architecture 
+		return (a[6]+a[7]+a[4]+a[5]+a[2]+a[3]+a[0]+a[1]);
+	} else {
+		throw "Unexpected memory Address size";
+	}
+}
